c7f4c132bdb4ef9c5cec7dbd857c778bf32eef1f
Endre Horvath <horvath.endre910@gmail.com>
2025-03-26 15:00:27 -0400

    Save: Task5

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,5 +1,5 @@
-
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -7,4 +7,11 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    emotion_scores["dominant_emotion"] = dominant_emotion
+
+    return emotion_scores
